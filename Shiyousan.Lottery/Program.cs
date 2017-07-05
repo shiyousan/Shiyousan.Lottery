04b52672886ad21dd033a36b2e2fcd0a36505751
ywfroad <ywfroad@hotmail.com>
2017-07-05 14:34:09 +0800

    添加十有三彩票项目

---
+++ Shiyousan.Lottery/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shiyousan.Lottery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start 31-of-7");
+            List<int> numberList = new List<int>();
+            for (int loop = 0; loop < 3; loop++)
+            {
+                for (int i = 0; i < 7; i++)
+                {
+                    int num = GetRandomNumber(numberList);
+                    numberList.Add(num);
+                }
+                Console.WriteLine("Winning Numbers：");
+                foreach (int num in numberList)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine("");
+                numberList.Clear();
+            }
+
+            Console.Read();
+        }
+
+        static int GetRandomNumber(List<int> numberList)
+        {
+            Random random = new Random();
+            int num = random.Next(1, 32);
+            if (!numberList.Contains(num))
+            {
+                return num;
+            }
+            else
+            {
+                return GetRandomNumber(numberList);
+            }
+        }
+    }
+}
